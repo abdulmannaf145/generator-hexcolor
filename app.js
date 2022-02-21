62f3e72c12a69045030c58ce4931262ea369cd96
abdulmannaf145 <mdabdulmannaf145@gmail.com>
2022-02-21 13:09:23 +0600

    Generate A Dynamic Toast Message

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
+let div = null;
 window.onload = () => {
     main();
 }
 
 function main() {
     const root = document.getElementById('root');
-    const btn = document.getElementById('btn');
+    const displayBgColor = document.getElementById('btn');
     const output = document.getElementById('output');
-    const copy = document.getElementById('copy-btn');
+    const copyBtn = document.getElementById('copy-btn');
 
-    btn.addEventListener('click', function () {
+    displayBgColor.addEventListener('click', function () {
         const bgColor = generateHaxColor();
         root.style.backgroundColor = bgColor;
         output.value = bgColor;
     });
-    copy.addEventListener('click',function () {
-        window.navigator.clipboard.writeText(output.value);
+    copyBtn.addEventListener('click',function () {
+        navigator.clipboard.writeText(output.value);
+        if (div !== null) {
+            div.remove();
+            div = null;
+        }
+        generateToastMessage(`${output.value} copied`);
     })
 }
 
@@ -24,5 +30,22 @@ function generateHaxColor() {
     const green = Math.floor(Math.random() * 255);
 
     return `#${red.toString(16)}${blue.toString(16)}${green.toString(16)}`
-    
+}
+
+function generateToastMessage(msg) {
+    div = document.createElement('div');
+    div.innerText = msg;
+    div.className = 'toast-message toast-message-slide-in';
+
+    div.addEventListener('click',function() {
+        div.classList.remove('toast-message-slide-in');
+        div.classList.add('toast-message-slide-out');
+
+
+        div.addEventListener('animationend',function () {
+            div.remove();
+            div = null;
+        })
+    })
+    document.body.appendChild(div);
 }
