aa63bc34b69414083563546bda1c25fe0ed3c45d
abdulmannaf145 <mdabdulmannaf145@gmail.com>
2022-02-21 08:24:05 +0600

    generator hex color

---
+++ app.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+    main();
+}
+
+function main() {
+    const root = document.getElementById('root');
+    const btn = document.getElementById('btn');
+    const output = document.getElementById('output');
+
+    btn.addEventListener('click',function () {
+        const bgColor = generateHaxColor();
+        root.style.backgroundColor = bgColor;
+        output.value = bgColor;
+    })
+}
+
+function generateHaxColor() {
+    const red = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+
+    return `#${red.toString(16)}${blue.toString(16)}${green.toString(16)}`
+    
+}
